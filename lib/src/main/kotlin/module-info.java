944c90879f0746a55ad7ebc2ae97f898e86c640b
RationalityFrontline <rationalityfrontline@gmail.com>
2022-11-28 17:29:10 +0800

    精简 module-info.java

--- lib/src/main/kotlin/module-info.java
+++ lib/src/main/kotlin/module-info.java
@@ -1,12 +1,7 @@
-@SuppressWarnings({"requires-transitive-automatic", "JavaRequiresAutoModule"})
 module ktrader.broker.ctp {
-    requires transitive kotlin.stdlib;
-    requires transitive kotlinx.coroutines.core.jvm;
-    requires transitive kevent;
-    requires transitive ktrader.api;
+    requires ktrader.api;
     requires ktrader.utils;
     requires jctp;
-    requires static org.pf4j;
 
     exports org.rationalityfrontline.ktrader.broker.ctp;
 }
